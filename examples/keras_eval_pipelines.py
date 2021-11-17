e5c58febd890d3d74312290c7e3ad0c5de7e5750
David Metz <d.metz@tu-braunschweig.de>
2021-11-17 19:48:06 +0100

    ajust colorbar in plot_compare_samples

--- examples/keras_eval_pipelines.py
+++ examples/keras_eval_pipelines.py
@@ -11,13 +11,14 @@ from eit_tf_workspace.train_utils.select_gen import select_gen
 
 
 from logging import getLogger
+
+from eit_tf_workspace.utils.path_utils import get_POSIX_path
 logger = getLogger(__name__)
 
-
-def std_eval_pipeline():
+def std_eval_pipeline(dir_path:str=''):
     logger.info('### Start standard evaluation ###')
 
-    metadata, raw_samples= reload_samples(MatlabSamples(), dir_path='')
+    metadata, raw_samples= reload_samples(MatlabSamples(), dir_path=dir_path)
     gen= select_gen(metadata)
     gen.load_model(metadata)
     gen.build_dataset(raw_samples, metadata)
@@ -32,16 +33,17 @@ def std_eval_pipeline():
     logger.info(f'Predicted perm shape: {nn_img_data.shape}')
     img_data.append(ImageDataset(nn_img_data, 'NN Predicted image',fwd_model))
 
-    eidors_img_data=load_eidors_solution(
-        metadata=metadata,
-        initialdir= os.path.split(metadata.raw_src_file[0])[0])
+    # eidors_img_data=load_eidors_solution(
+    #     metadata=metadata,
+    #     initialdir= os.path.split(metadata.raw_src_file[0])[0])
     
-    for p in eidors_img_data:
-        img_data.append(ImageDataset(p[0], p[1],fwd_model))
+    # for p in eidors_img_data:
+    #     img_data.append(ImageDataset(p[0], p[1],fwd_model))
 
     img_data = trunc_img_data_nb_samples(img_data, max_nb=100) 
     results = compute_eval(img_data) 
     
+    plot_compare_samples(image_data=img_data, nb_samples=5, rand=True, orient=Orientation.Portrait)
     plot_compare_samples(image_data=img_data, nb_samples=5, rand=True, orient=Orientation.Landscape)
     # plot_real_NN_EIDORS(gen.getattr_dataset('fwd_model'), true_img_data[randnlist,:].T, nn_img_data[randnlist,:].T)
     plot_eval_results(results, axis='linear')
@@ -52,6 +54,8 @@ if __name__ == "__main__":
     main_log()
     change_level(logging.DEBUG)
 
-    std_eval_pipeline()
+
+    dir_path= 'E:\EIT_Project\05_Engineering\04_Software\Python\eit_tf_workspace\outputs\Std_keras_test_20211117_165710'
+    std_eval_pipeline(get_POSIX_path(dir_path))
     plt.show()
     
