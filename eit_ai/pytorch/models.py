293c9c093e527f5f36f033deaaafb9f4faebd8a6
Jiawei Zhang <jiawei.ryan.zhang@outlook.com>
2021-12-02 01:39:06 +0100

    rewrite

--- eit_ai/pytorch/models.py
+++ eit_ai/pytorch/models.py
@@ -3,7 +3,7 @@ import os
 from enum import Enum
 from logging import getLogger
 from typing import Any
-
+from contextlib import redirect_stdout
 import numpy as np
 import torch
 from eit_ai.pytorch.const import PYTORCH_LOSS, PYTORCH_OPTIMIZER, PytorchLosses, PytorchOptimizers
@@ -22,19 +22,24 @@ from torch.utils.data import DataLoader
 
 logger = getLogger(__name__)
 
-
+MODEL_SUMMARY_FILENAME='model_summary'
 
 ################################################################################
 # Std PyTorch ModelManager
 ################################################################################
-class StdTorchModel(nn.Module):
+class StdPytorchModel(nn.Module):
     def __init__(self, metadata: MetaData) -> None:
-        super(StdTorchModel, self).__init__()
+        super(StdPytorchModel, self).__init__()
         in_size=metadata.input_size
         out_size=metadata.output_size
-        self.linear1 = nn.Linear(in_size, 3)
-        self.linear2 = nn.Linear(3, out_size)
-        self.relu = nn.ReLU()
+        # self.linear1 = nn.Linear(in_size, 3)
+        # self.linear2 = nn.Linear(3, out_size)
+        # self.relu = nn.ReLU()
+        self.model = torch.nn.Sequential()
+        self.model.add_module('dense1', nn.Linear(in_size, 512))
+        self.model.add_module('relu', nn.ReLU())
+        self.model.add_module('dense2', nn.Linear(512, out_size))
+        self.model.add_module('sigmoid', nn.Sigmoid())
 
     def prepare(self, op:torch.optim.Optimizer, loss):
         self.optimizer= op
@@ -42,30 +47,23 @@ class StdTorchModel(nn.Module):
         
     def forward(self, dataloader):
         
-        # self.specific_var['optimizer']= get_pytorch_optimizer(metadata)
-        # self.specific_var['loss'] = get_pytorch_loss(metadata)
-        # loss= nn.MSELoss()
-        # optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
-    
-        # batch = [b.cuda() for b in batch] # if needs GPU
-        inputs,  labels = dataloader
-        x = self.relu(self.linear1(inputs))
-        out = self.relu(self.linear2(x))
+        # inputs,  labels = dataloader
+        # x = self.relu(self.linear1(inputs))
+        return self.model(dataloader)
         
-        self.loss = self.loss(out, labels)
-        self.optimizer.zero_grad() 
-        self.loss.backward()
-        self.optimizer.step() 
+        # self.loss = self.loss(out, labels)
+        # self.optimizer.zero_grad() 
+        # self.loss.backward()
+        # self.optimizer.step() 
     
 
-    def predict(self, x):
+    def predict(self, x_pred: np.ndarray):
         """[summary]
-        """        
-
-        inputs = x
-        x = self.relu(self.linear1(inputs))
-        out = self.relu(self.linear2(x))
-        return out
+        """
+        self.model.eval()
+        with torch.no_grad:
+            out = self.model(torch.Tensor(x_pred))        
+            return out
 class StdPytorchModelManager(Models):
 
     # model=None
@@ -80,8 +78,8 @@ class StdPytorchModelManager(Models):
         Args:
             metadata (MetaData):
         """
-        self.model= StdTorchModel(metadata=metadata)
-        self.name= 'StdTorch'
+        self.model= StdPytorchModel(metadata=metadata)
+        self.name= 'StdPytorch'
 
  
     def _get_specific_var(self, metadata:MetaData)-> None:
@@ -103,9 +101,9 @@ class StdPytorchModelManager(Models):
             WrongMetrixError: raised if passed metadata.metrics is not a list #Could be better tested... TODO
             WrongLearnRateError: raised if passed metadata.learning_rate >= 1.0 
         """     
-        self.specific_var['optimizer'] = get_torch_optimizer(metadata)
+        self.specific_var['optimizer'] = get_pytorch_optimizer(metadata)
         # self.specific_var['lr'] = metadata.learning_rate
-        self.specific_var['loss'] = get_torch_loss(metadata)
+        self.specific_var['loss'] = get_pytorch_loss(metadata)
         if not isinstance(metadata.metrics, list):
             raise WrongMetricsError(f'Wrong metrics type: {metadata.metrics}')
         self.specific_var['metrics'] = metadata.metrics   
@@ -113,7 +111,7 @@ class StdPytorchModelManager(Models):
 
     def _prepare_model(self)-> None:
         """3. method called during building:
-        set the model ready to train (in torch this step is compiling)
+        set the model ready to train (in pytorch this step is compiling)
         using "specific_var"
 
         Raises:
@@ -136,14 +134,25 @@ class StdPytorchModelManager(Models):
             ModelNotDefinedError: if "self.model" is not a Model type
             ModelNotPreparedError: if "self.model" is not compiled or similar
         """ 
-        gen=DataloaderGenerator_old()
+        gen=DataloaderGenerator()
         train_dataloader=gen.make(dataset, 'train', metadata=metadata)
 
-        for e in range(metadata.epoch):
-            logger.info(f'Epoch {e}/{metadata.epoch}')
+        for epoch in range(metadata.epoch):
+            logger.info(f'Epoch {epoch}/{metadata.epoch}')
             for idx, data_i in enumerate(train_dataloader):
                 logger.debug(f'Batch {idx}')
-                self.model.forward(data_i)
+                inputs, labels = data_i
+                y_pred = self.model(inputs)
+                loss_value = self.model.loss(y_pred, labels)
+               
+                self.model.optimizer.zero_grad()
+                loss_value.backward()
+                self.model.optimizer.step() 
+                if (epoch+1) % 20 == 0:
+                    print('Epoch[{}/{}], loss: {:.6f}'.format(epoch+1,
+                                                  metadata.epoch,
+                                                  loss_value.item()))
+                
                 
 
 
@@ -169,7 +178,14 @@ class StdPytorchModelManager(Models):
                         predicted samples values
         """
         # X_pred preprocess if needed
-        return self.model.predict(X_pred, metadata)  
+        if X_pred.shape[0]==1:
+            return self.model.predict(X_pred)
+        else:
+            res = []
+            for i in range(X_pred.shape[0]):
+                pred = self.model.predict(X_pred[[i], :]).numpy().tolist()
+                res.append[pred]
+                return res  
 
 
     def save(self, metadata:MetaData)-> str:
@@ -182,7 +198,7 @@ class StdPytorchModelManager(Models):
             str: the saving path which is automatically set using
             metadata.dirpath
         """     
-        return save_torch_model(self.model, dir_path=metadata.dir_path)
+        return save_pytorch_model(self.model, dir_path=metadata.dir_path, save_summary=metadata.save_summary)
 
 
 
@@ -192,15 +208,15 @@ class StdPytorchModelManager(Models):
         Args:
             metadata (MetaData)
         """  
-        self.model=load_torch_model(dir_path=metadata.dir_path)
+        self.model= load_pytorch_model(dir_path=metadata.dir_path)
 
 
 ################################################################################
 # common methods
 ################################################################################
-PYTORCH_MODEL_SAVE_FOLDERNAME= 'model.torch'
+PYTORCH_MODEL_SAVE_FOLDERNAME= 'model.pytorch'
 
-def assert_torch_model_defined(model:Any)-> nn.Module:
+def assert_pytorch_model_defined(model:Any)-> nn.Module:
     """allow to react if model not  defined
 
     Args:
@@ -210,14 +226,14 @@ def assert_torch_model_defined(model:Any)-> nn.Module:
         ModelNotDefinedError: [description]
 
     Returns:
-        torch.models.Model: [description]
+        pytorch.models.Model: [description]
     """    
     if not isinstance(model, nn.Module):
         raise ModelNotDefinedError(f'Model has not been correctly defined: {model}')
     return model
 
 
-def get_torch_optimizer(metadata:MetaData, net:nn.Module)-> torch.optim.Optimizer:
+def get_pytorch_optimizer(metadata:MetaData, net:nn.Module)-> torch.optim.Optimizer:
 
     if not metadata.optimizer:
         metadata.optimizer=list(PYTORCH_OPTIMIZER.keys())[0].value
@@ -230,9 +246,9 @@ def get_torch_optimizer(metadata:MetaData, net:nn.Module)-> torch.optim.Optimize
         if metadata.learning_rate >= 1.0:
             raise WrongLearnRateError(f'Wrong learning rate type (>= 1.0): {metadata.learning_rate}')
         return optimizer(net.parameters(), lr= metadata.learning_rate)
-    return optimizer(net.parameters())
+    return optimizer(net.parameters(), lr=metadata.learning_rate)
 
-def get_torch_loss(metadata:MetaData):
+def get_pytorch_loss(metadata:MetaData):
 
     if not metadata.loss:
         metadata.loss=list(PYTORCH_LOSS.keys())[0].value
@@ -243,39 +259,39 @@ def get_torch_loss(metadata:MetaData):
 
     return loss
 
-def save_torch_model(model:nn.Module, dir_path:str='', save_summary:bool=False)-> str:
-    """Save a torch model, additionnaly can be the summary of the model be saved"""
+def save_pytorch_model(model:nn.Module, dir_path:str='', save_summary:bool=False)-> str:
+    """Save a pytorch model, additionnaly can be the summary of the model be saved"""
     if not isdir(dir_path):
         dir_path=os.getcwd()
     model_path=os.path.join(dir_path, PYTORCH_MODEL_SAVE_FOLDERNAME)
     
-    model.save(model_path)
+    torch.save(model, model_path)
 
-    logger.info(f'torch model saved in: {model_path}')
+    logger.info(f'pytorch model saved in: {model_path}')
     
-    # if save_summary:
-    #     summary_path= os.path.join(dir_path, const.MODEL_SUMMARY_FILENAME)
-    #     with open(summary_path, 'w') as f:
-    #         with redirect_stdout(f):
-    #             model.summary()
-    #     logger.info(f'torch model summary saved in: {summary_path}')
+    if save_summary:
+        summary_path= os.path.join(dir_path, MODEL_SUMMARY_FILENAME)
+        with open(summary_path, 'w') as f:
+            with redirect_stdout(f):
+                model.summary()
+        logger.info(f'pytorch model summary saved in: {summary_path}')
     
     return model_path
 
-def load_torch_model(dir_path:str='') -> nn.Module:
-    """Load torch Model and return it if succesful if not """
+def load_pytorch_model(dir_path:str='') -> nn.Module:
+    """Load pytorch Model and return it if succesful if not """
 
     if not isdir(dir_path):
-        logger.info(f'torch model loading - failed, wrong dir {dir_path}')
+        logger.info(f'pytorch model loading - failed, wrong dir {dir_path}')
         return
     model_path=os.path.join(dir_path, PYTORCH_MODEL_SAVE_FOLDERNAME)
     if not isdir(model_path):
-        logger.info(f'torch model loading - failed, {PYTORCH_MODEL_SAVE_FOLDERNAME} do not exist in {dir_path}')
+        logger.info(f'pytorch model loading - failed, {PYTORCH_MODEL_SAVE_FOLDERNAME} do not exist in {dir_path}')
         return None
     try:
-        model:nn.Module = torch.load(model_path, custom_objects=ak.CUSTOM_OBJECTS)
-        logger.info(f'torch model loaded: {model_path}')
-        logger.info('torch model summary:')
+        model:nn.Module = torch.load(model_path)
+        logger.info(f'pytorch model loaded: {model_path}')
+        logger.info('pytorch model summary:')
         model.summary()
         return model
     except BaseException as e: 
@@ -288,7 +304,7 @@ def load_torch_model(dir_path:str='') -> nn.Module:
 
 
 PYTORCH_MODELS={
-    PytorchModels.StdTorchModel: StdTorchModel,
+    PytorchModels.StdPytorchModel: StdPytorchModel,
 }
 
 
